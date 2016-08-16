07873c4d1eeba90a78a42a9722f6a8cccf344eb6
Emil Abraham <emilabraham1414@gmail.com>
2016-08-16 12:31:55 -0400

    Adds tests
    Adds some basic tests

--- app/app.spec.js
+++ app/app.spec.js
@@ -6,9 +6,18 @@ describe('PhoneListController', function() {
 
   it('should create a `phones` model with 3 phones', inject(function($controller) {
     var scope = {};
+    //Finds controller by name
     var ctrl = $controller('PhoneListController', {$scope: scope});
 
     expect(scope.phones.length).toBe(3);
+  }));
+
+  it('should have a variable with value world', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', {$scope: scope});
+
+    expect(scope.name).toBe('world');
+
   }));
 
 });
